49b109d293ac531780728c45cc0ded2caf8ef6f6
47 Ronin <33853447+ronibd0@users.noreply.github.com>
2023-04-28 13:29:11 +0600

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Ronibd0.
+      <time>{YEAR}</time> © RemoteJS.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
