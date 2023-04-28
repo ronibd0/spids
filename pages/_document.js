8fd1fccada4e43dd60ce64cff99ddfea028645ca
47 Ronin <33853447+ronibd0@users.noreply.github.com>
2023-04-28 12:53:39 +0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Remote job help center',
+    description: 'Get hired easier than ever in Upwork and Freelancer',
+    image: 'https://goisp.net/drive/service/remote job help desk.png'
   }
 
   return (
